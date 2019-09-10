25c661fdffb71e19c2b82897ff91c4dc808e833a
Alejandro H Tadino M <alejandrohtadinom@gmail.com>
2019-09-10 17:55:06 -0400

    [UPD] Header web design 3.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,79 @@
 <template>
-  <div class="home">
-    <header class="header-container">
-      <div class="header-content">
-        <h1 class="page-title">Alejandro H Tadino M</h1>
-        <h3 class="tag-line">Programmer // Developer // Thinker</h3>
-        <div class="cta-container">
-          <a class="cta" href="/about">Sobre mi</a>
-          <a class="cta" href="/contact">Contacto</a>
-        </div>
-      </div>
-    </header>
-    <main>
-      <section id="service">
-        <div class="card">
-          <figure>
-            <img src="@/assets/img/austin-distel-VwsuhJ9uee4-unsplash.jpg" alt="Photo by Carlos Muza on Unsplash">
-            <figcaption></figcaption>
-          </figure>
-        </div>
-        <div class="card">
-          <figure>
-            <img src="" alt="">
-            <figcaption></figcaption>
-          </figure>
-        </div>
-        <div class="card">
-          <figure>
-            <img src="" alt="">
-            <figcaption></figcaption>
-          </figure>
-        </div>
-        <div class="card">
-          <figure>
-            <img src="" alt="">
-            <figcaption></figcaption>
-          </figure>
-        </div>
-      </section>
-      <section id="about">
-        <h2>Alejandro H Tadino M</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, neque. Vitae dolores fuga sit rem distinctio quis, natus beatae est inventore consequuntur porro ullam minima non similique aliquam, delectus quia.
-        </p>
-      </section>
-      <section id="contact">
-        <h3>contactos</h3>
-      </section>
-    </main>
-  </div>
+	<div class="home">
+		<header class="header-container">
+			<div class="header-rect"></div>
+			<div class="header-sub">
+				<h3 class="tag-line">Programmer // Developer // Thinker</h3>
+				<div class="cta-container">
+					<a class="cta" href="/about">Sobre mi</a>
+					<a class="cta" href="/contact">Contacto</a>
+				</div>
+			</div>
+			<div class="header-title">
+				<h1 class="page-title">Alejandro H Tadino M</h1>
+			</div>
+		</header>
+		<main>
+			<section id="service">
+				<div class="card">
+					<figure>
+						<img src="@/assets/img/card-1.jpg" alt="Photo by Carlos Muza on Unsplash">
+						<figcaption>
+							<h3 class="card-tag">Poscionamiento en la Web</h3>
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, perspiciatis vitae corrupti possimus, itaque praesentium laborum beatae nulla dolorem porro ab repellendus numquam. Repellendus aperiam animi ducimus suscipit. Excepturi, cupiditate!</p>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="card">
+					<figure>
+						<img src="@/assets/img/card-2.jpg" alt="">
+						<figcaption>
+							<h3 class="card-tag">Diseño de aplicaciones</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas eveniet cupiditate ullam, doloribus, modi voluptatum molestiae repellat voluptatem perspiciatis recusandae nobis laborum dolor velit fugiat debitis omnis nihil obcaecati.</p>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="card">
+					<figure>
+						<img src="" alt="">
+						<figcaption>
+							<h3 class="card-tag">tu blog a tu manera</h3>
+							<p>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi nulla fuga nostrum explicabo voluptate. Vel, assumenda incidunt numquam repudiandae eum dolores esse quasi fugiat laudantium soluta non, debitis eius inventore.
+							</p>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="card">
+					<figure>
+						<img src="" alt="">
+						<figcaption>
+							<h3 class="card-tag">tu tienda oline ya</h3>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic quam nesciunt quasi rerum assumenda commodi accusamus inventore modi! Laboriosam, in nam consectetur ut pariatur voluptates quibusdam culpa harum quis!
+							</p>
+						</figcaption>
+					</figure>
+				</div>
+			</section>
+			<section id="about">
+				<h2>Alejandro H Tadino M</h2>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, neque. Vitae dolores fuga sit rem distinctio quis, natus beatae est inventore consequuntur porro ullam minima non similique aliquam, delectus quia.
+				</p>
+			</section>
+			<section id="contact">
+				<h3>contactos</h3>
+			</section>
+		</main>
+	</div>
 </template>
 
 <script>
 
 export default {
-  name: 'home',
-  components: {
-  }
+	name: 'home',
+	components: {
+	}
 }
 </script>
