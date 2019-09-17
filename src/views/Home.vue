cd11f986611d241f166c8ee52af618e326c6debc
Alejandro H Tadino M <alejandrohtadinom@gmail.com>
2019-09-17 18:33:20 -0400

    [ADD] Profile section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,15 +21,15 @@
 			<section id="products">
 				<div class="product">
 					<img src="@/assets/img/svg/004-startup.svg" alt="">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam itaque minus, nulla ducimus totam tenetur quos eveniet sequi praesentium pariatur quasi, odio, recusandae obcaecati quo aliquid eaque vero incidunt accusantium!</p>
+					<h2>Posicionamiento web</h2>
 				</div>
 				<div class="product">
 					<img src="@/assets/img/svg/012-laptop-1.svg" alt="">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nihil voluptatibus voluptas debitis autem amet ipsam laboriosam quibusdam quaerat ipsum. Quos voluptatem laborum labore natus dicta, debitis quis, cum veritatis.</p>
+					<h2>Aplicacioens en linea</h2>
 				</div>
 				<div class="product">
 					<img src="@/assets/img/svg/006-user.svg" alt="">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente assumenda architecto optio quibusdam, accusamus quidem voluptatem quasi suscipit consectetur ipsa illo dolorem laboriosam minus. Vitae earum impedit dolorum quia repellat.</p>
+					<h2>Asesorameinto tecnologico</h2>
 				</div>
 			</section>
 			<section id="services">
@@ -38,7 +38,9 @@
 						<img src="@/assets/img/card-1.jpg" alt="Photo by Carlos Muza on Unsplash">
 						<figcaption>
 							<h3 class="service-tag">Poscionamiento en la Web</h3>
-							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, perspiciatis vitae corrupti possimus, itaque praesentium laborum beatae nulla dolorem porro ab repellendus numquam. Repellendus aperiam animi ducimus suscipit. Excepturi, cupiditate!</p>
+							<p>
+                  Posicione su negocio, producto o servicio en las primeras paginas de los buscadores y destaque entre su competencia.
+              </p>
 						</figcaption>
 					</figure>
 				</div>
@@ -47,7 +49,9 @@
 						<img src="@/assets/img/card-2.jpg" alt="">
 						<figcaption>
 							<h3 class="service-tag">Diseño de aplicaciones</h3>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas eveniet cupiditate ullam, doloribus, modi voluptatum molestiae repellat voluptatem perspiciatis recusandae nobis laborum dolor velit fugiat debitis omnis nihil obcaecati.</p>
+							<p>
+                  Diseña tu pagina web a tu manera.
+              </p>
 						</figcaption>
 					</figure>
 				</div>
@@ -55,10 +59,10 @@
 					<figure>
 						<img src="@/assets/img/card-3.jpg" alt="">
 						<figcaption>
-							<h3 class="service-tag">tu blog a tu manera</h3>
+							<h3 class="service-tag">Tu blog a tu manera</h3>
 							<p>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi nulla fuga nostrum explicabo voluptate. Vel, assumenda incidunt numquam repudiandae eum dolores esse quasi fugiat laudantium soluta non, debitis eius inventore.
-							</p>
+                  Inicia tu carrera Blogger con el pie derecho, diseño, asesoramiento e instalacion de Wordpress, Joomla, Drupal, Blogger...
+              </p>
 						</figcaption>
 					</figure>
 				</div>
@@ -66,22 +70,87 @@
 					<figure>
 						<img src="@/assets/img/card-4.jpg" alt="">
 						<figcaption>
-							<h3 class="service-tag">tu tienda oline ya</h3>
+							<h3 class="service-tag">Tu propia tienda en linea</h3>
 							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic quam nesciunt quasi rerum assumenda commodi accusamus inventore modi! Laboriosam, in nam consectetur ut pariatur voluptates quibusdam culpa harum quis!
-							</p>
+                  Tener tu propio negocio en linea nunca fue tan facil.
+              </p>
 						</figcaption>
 					</figure>
 				</div>
 			</section>
 			<section id="about">
-				<h2>Alejandro H Tadino M</h2>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, neque. Vitae dolores fuga sit rem distinctio quis, natus beatae est inventore consequuntur porro ullam minima non similique aliquam, delectus quia.
-				</p>
+          <div class="bio-img">
+              <div class="about-img about-img about-img-1">
+                  <img alt="" src="@/assets/img/bio-1.jpg"/>
+              </div>
+              <div class="about-img about-img-2">
+                  <img alt="" src="@/assets/img/bio-2.jpg"/>
+              </div>
+              <div class="about-img about-img-3">
+                  <img alt="" src="@/assets/img/bio-3.jpg"/>
+              </div>
+              <div class="about-img about-img-4">
+                  <img alt="" src="@/assets/img/bio-4.jpg"/>
+              </div>
+              <div class="about-img about-img-5">
+                  <img alt="" src="@/assets/img/bio-5.jpg"/>
+              </div>
+              <div class="about-img about-img-6">
+                  <img alt="" src="@/assets/img/bio-6.jpg"/>
+              </div>
+              <div class="about-img about-img-7">
+                  <img alt="" src="@/assets/img/bio-7.jpg"/>
+              </div>
+              <div class="about-img about-img-8">
+                  <img alt="" src="@/assets/img/bio-8.jpg"/>
+              </div>
+              <div class="about-img about-img-9">
+                  <img alt="" src="@/assets/img/bio-9.jpg"/>
+              </div>
+              <div class="about-img about-img-10">
+                  <img alt="" src="@/assets/img/bio-10.jpg"/>
+              </div>
+              <div class="about-img about-img-11">
+                  <img alt="" src="@/assets/img/bio-11.jpg"/>
+              </div>
+          </div>
+          <div class="bio-main">
+              <img alt="" src="@/assets/img/bio-main.jpg"/>
+              <div class="about-text">
+				          <h2>Alejandro H Tadino M</h2>
+				          <p>
+                    Hola, mi nombre es alejandro, soy un desarrollador y diseñador de aplicaciones web con mas de 10 años de experiencia en el campo de IT.
+                    <br />
+                    <br />
+                    Me gusta desarrollar ideas y llevar a cabo proyectos para mis clientes que realmente ofrezcan una ventaja para su negocio o empresa en el mundo digital.
+                    <br />
+                    <br />
+                    Para mi, cada proyecto es una alianza de la que podemos aprender nuevas estrategias para alcanzar las metas trazadas.
+                  </p>
+              </div>
+          </div>
 			</section>
 			<section id="contact">
-				<h3>contactos</h3>
+				<h2>contactos</h2>
+        <div class="contact-sns">
+            <h3>Redes</h3>
+            twitter
+            github
+            gitlab
+            linkedin
+            facebook
+            instagram
+        </div>
+        <div class="contact-form">
+            <label for="full-name">Full Name</label>
+            <input type="text" name="name" id="full-name" placeholder="Nombre" required="true">
+            <label for="email-address">Correo</label>
+            <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="true">
+            <label for="message">Mensaje</label>
+            <textarea rows="5" name="message" id="message" placeholder="" required=""></textarea>
+            <input type="hidden" name="_subject" id="email-subject" value="Contacto desde alejandrohtadinom.com">
+            <input type="button" value="Submit">
+            </div>
 			</section>
 		</main>
 	</div>
