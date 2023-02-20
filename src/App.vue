0ad9c29b05d44991f1115709201206d7d9d18910
Alejandro H Tadino M <tadinomalejandroh@gmail.com>
2023-02-20 18:58:52 -0400

    [UPG] Version 3.0

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div id="app">
-    <baseNavbarComponent />
-    <router-view/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
-<style lang="scss">
-  @import "./styles/global.scss";
-</style>
-<script>
-  import baseNavbarComponent from '@/components/base/baseNavbarComponent.vue'
 
-  export default {
-    name: 'app',
-    components: {
-      baseNavbarComponent
-    }
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from './components/HelloWorld.vue'
+
+@Options({
+  components: {
+    HelloWorld
   }
+})
+export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
-const path = require("path");
-
-module.exports = {
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
-    }
-  }
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
